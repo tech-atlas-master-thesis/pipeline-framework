fb9cf191068bb3e1c228a5fbd791f56854ff6cb6
Fabian Mooslechner <fabian@mooslechner.dev>
2026-07-26 21:46:20 +0200

    Add time selection to projects

--- server/dto/serialisation.py
+++ server/dto/serialisation.py
@@ -1,7 +1,12 @@
+import datetime
+
+import pandas as pd
 from bson import ObjectId
 
 
 def custom_json_encoder(obj):
     if isinstance(obj, ObjectId):
         return {"$oid": str(obj)}
+    if isinstance(obj, pd.Timestamp):
+        return obj.isoformat()
     raise TypeError(f"{repr(obj)} of type {type(obj)} is not JSON serializable")
